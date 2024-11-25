6850cb148f1e89ae322c8f04e70cf62743c2dec1
ditharaavindi <it23424432@my.sliit.lk>
2024-11-25 20:45:04 +0530

    update EmployeRepository

--- backend/src/main/java/backend/repository/EmployeRepository.java
+++ backend/src/main/java/backend/repository/EmployeRepository.java
@@ -1,5 +1,10 @@
 package backend.repository;
 
+import backend.model.Employe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 public interface EmployeRepository extends JpaRepository<Employe, Long> {
     boolean existsByEmail(String email);
 
