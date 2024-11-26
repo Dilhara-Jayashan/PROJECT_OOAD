f839fa872500a3dcf5688597a0681497307ca390
ditharaavindi <it23424432@my.sliit.lk>
2024-11-26 16:41:11 +0530

    update AddEmployee

---
+++ frontend/src/Components/Employee/img/AddEmployee.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom'; // for navigation
+import axios from 'axios'; // for API requests
+
+function AddEmployee() {
+
+}
+
+export default AddEmployee
