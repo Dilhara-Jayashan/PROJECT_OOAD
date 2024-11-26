76a9b5d9eb8404ffa2c10fe2027b25379e0e71ab
ditharaavindi <it23424432@my.sliit.lk>
2024-11-26 16:48:14 +0530

    update on AddEmployee

--- frontend/src/Components/Employee/img/AddEmployee.js
+++ frontend/src/Components/Employee/img/AddEmployee.js
@@ -5,6 +5,48 @@ import axios from 'axios'; // for API requests
 
 function AddEmployee() {
 
+    const [employee, setEmployee] = useState({
+        empid: '',
+        name: '',
+        email: '',
+        password: '',
+        phone: '',
+        birthday: '',
+        address: '',
+        roll: '',
+    });
+    const navigate = useNavigate(); // to navigate after success
+    // Function to generate random manager ID
+    const generateCode = (type) => {
+        const randomNumbers = Math.floor(10000 + Math.random() * 90000);
+        return `EMPID${randomNumbers}`;
+    };
+    useEffect(() => {
+        setEmployee((prevInputs) => ({
+            ...prevInputs,
+            empid: generateCode(),
+        }));
+    }, []);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEmployee((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+
+        }
+        catch (error) {
+            
+        }
+    };
+
+
+    return (
+    )
 }
 
 export default AddEmployee
